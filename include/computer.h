4bbc9d3c6a5a409e6a7927d25e8b2eb466dbbc9d
nkien0204 <44253178+nkien0204@users.noreply.github.com>
2020-09-24 13:31:14 +0700

    Update computer.h

--- include/computer.h
+++ include/computer.h
@@ -18,12 +18,12 @@ public:
 	Computer(const int &level, const Board *hum_board, const vector<Ship *> &hum_ships);
 	void createBoard(const Board *hum_board);
 	void createShips(const vector<Ship *> &hum_ships);
-	void choosePosition(unsigned int &x,unsigned int &y, const vector<vector<int>> matrix);
+	void choosePosition(int &x, int &y, const vector<vector<int>> matrix);
 	~Computer();
 private:
-	void playEasyLv(unsigned int &x,unsigned int &y, const vector<vector<int>> matrix);
-	void playMediumLv(unsigned int &x,unsigned int &y, const vector<vector<int>> matrix);
-	void playHardLv(unsigned int &x,unsigned int &y, const vector<vector<int>> matrix);
+	void playEasyLv(int &x, int &y, const vector<vector<int>> matrix);
+	void playMediumLv(int &x, int &y, const vector<vector<int>> matrix);
+	void playHardLv(int &x, int &y, const vector<vector<int>> matrix);
 };
 
 #endif
