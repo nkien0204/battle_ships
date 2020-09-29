04db527651f0a8bbde3d3b8dfe5fc7efc6717017
nkien0204 <44253178+nkien0204@users.noreply.github.com>
2020-09-29 11:50:42 +0700

    Update human.cpp

--- src/human.cpp
+++ src/human.cpp
@@ -12,7 +12,7 @@ Human::Human() {
 	createShips();
 }
 
-void Human::choosePosition(int &x, int &y, const vector<vector<int>> matrix) {
+void Human::choosePosition(int &x, int &y, const vector<vector<int>> matrix, bool &flag) {
 	bool choosing = true;
 	string s_x, s_y;
 
@@ -46,57 +46,77 @@ void Human::createShips() {
 	int n_ships;
 	string s_n_ships;
 
-	while (choosing) {
-		try {
-			cout << "Enter number of ships: "; cin >> s_n_ships;	n_ships = stoi(s_n_ships);
-			if (n_ships <=0) {throw InputException();}
-			else choosing = false;
-		} catch (InputException ie) {
-			cout << ie.what() << endl;
-		} catch (...) {
-			cout << "Invalid input: Number of ships must be a number" << endl;
-		}
-	}
-
-	choosing = true;
-
-	while (choosing) {
-		try {
-			cout << "Ships: Using default size (1x3)? (y = Yes, n = No): "; cin >> choice;
-			if (choice == 'y' || choice == 'Y') {
-				for (int i = 0; i < n_ships; i++) {
-					Ship *my_ship = new Ship(S_LENGTH, S_WIDTH);
-					ships.push_back(my_ship);
-				}
-				choosing = false;
-			} else if (choice == 'n' || choice == 'N') {
-				for (int i = 0; i < n_ships; i++) {
-					cout << "Size of Ship[" << i << "] (0:(3x1), 1:(5x1), 2:(7x1)) = "; cin >> choice;
-					if (choice != '0' && choice != '1' && choice != '2') {
-						i--;
-						throw InputException();
-					} else if (choice == '0') {
-						Ship *my_ship31 = new Ship(3, 1);
-						ships.push_back(my_ship31);
-					} else if (choice == '1') {
-						Ship *my_ship51 = new Ship(5, 1);
-						ships.push_back(my_ship51);
-					} else if (choice == '2') {
-						Ship *my_ship71 = new Ship(7, 1);
-						ships.push_back(my_ship71);
-					}
-				}
-				choosing = false;
-			} else {
-				throw InputException();
+	while (1) {
+		while (choosing) {
+			try {
+				cout << "Enter number of ships: "; cin >> s_n_ships;	n_ships = stoi(s_n_ships);
+				if (n_ships <=0) {throw InputException();}
+				else choosing = false;
+			} catch (InputException ie) {
+				cout << ie.what() << endl;
+			} catch (...) {
+				cout << "Invalid input: Number of ships must be a number" << endl;
 			}
-		} catch (InputException ie) {
-			cout << ie.what() << endl;
 		}
-	}
 
-	for (int i = 0; i < ships.size(); i++) {
-		hp += ships[i]->getLength() * ships[i]->getWidth();
+		choosing = true;
+
+		while (choosing) {
+			try {
+				cout << "Ships: Using default size (1x3)? (y = Yes, n = No): "; cin >> choice;
+				if (choice == 'y' || choice == 'Y') {
+					for (int i = 0; i < n_ships; i++) {
+						Ship *my_ship = new Ship(S_LENGTH, S_WIDTH);
+						ships.push_back(my_ship);
+					}
+					choosing = false;
+				} else if (choice == 'n' || choice == 'N') {
+					for (int i = 0; i < n_ships; i++) {
+						try {
+							cout << "Size of Ship[" << i << "] (0:(3x1), 1:(5x1), 2:(7x1)) = "; cin >> choice;
+							if (choice != '0' && choice != '1' && choice != '2') {
+								i--;
+								throw InputException();
+							} else if (choice == '0') {
+								Ship *my_ship31 = new Ship(3, 1);
+								ships.push_back(my_ship31);
+							} else if (choice == '1') {
+								Ship *my_ship51 = new Ship(5, 1);
+								ships.push_back(my_ship51);
+							} else if (choice == '2') {
+								Ship *my_ship71 = new Ship(7, 1);
+								ships.push_back(my_ship71);
+							}
+						} catch (InputException ie) {
+							cout << ie.what() << endl;
+						}
+					}
+					choosing = false;
+				} else {
+					throw InputException();
+				}
+			} catch (InputException ie) {
+				cout << ie.what() << endl;
+			}
+		}
+
+		for (int i = 0; i < ships.size(); i++) {
+			hp += ships[i]->getLength() * ships[i]->getWidth();
+		}
+		cout << "hp: " << hp << endl;
+
+		if (hp <= (board->getHeight() * board->getWidth())) {
+			break;
+		} else {
+			cout << "Could not put ships on board, please resize the ships" << endl;
+			hp = 0;
+			choosing = true;
+			int size = ships.size();
+			for (int i = 0; i < size; i++) {
+				delete ships[0];
+				ships.erase(ships.begin());
+			}
+		}
 	}
 }
 
